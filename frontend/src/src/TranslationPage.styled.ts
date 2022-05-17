fd558751693ff2baef383003f7cb5f57bfbfa394
i.kondratev <i.kondratev1@g.nsu.ru>
2022-05-17 15:07:19 +0700

    changed page layout to grid

--- frontend/src/src/TranslationPage.styled.ts
+++ frontend/src/src/TranslationPage.styled.ts
@@ -1,45 +1,62 @@
 import styled, {css} from 'styled-components';
 
 const RootPageContainer = styled.div`
-  display: flex;
+  display: grid;
   flex-grow: 1;
-  padding: 20px;
-  gap: 20px;
+  padding: 1vw;
   background: #ececec;
+
+  grid-template-columns: 1fr 2fr;
+  grid-template-rows: 2fr 1fr;
+  gap: 1vw;
   
-  > * { flex-grow: 1; }
+  * {font-size: max(2vh, 12px);}
 `;
 
 type LinearContainerProps = {stretchingAlong?: boolean, gap?: number};
 const LinearContainer = styled.div`${(props: LinearContainerProps) => css`
   display: flex;
   align-items: stretch;
-  gap: ${props.gap ?? 0}px;
   
   ${props.stretchingAlong && css`
     > * { flex-grow: 1; }
   `}
 `}`;
 
-const Column = styled(LinearContainer)`
+const Column = styled(LinearContainer)`${(props: LinearContainerProps) => css`
   flex-direction: column;
 
-  background: green;
-`;
+  gap: ${props.gap ?? 0}vh;
+`}`;
 
-const Row = styled(LinearContainer)`
+const Row = styled(LinearContainer)`${(props: LinearContainerProps) => css`
   flex-direction: row;
-  
-  background: blue;
-`;
+
+  gap: ${props.gap ?? 0}vw;
+`}`;
 
 const Options = styled.div`
   display: flex;
-  background: black;
-  min-width: 100px;
-  min-height: 50px;
-  //align-self: center;
-  margin: 10px;
+  flex-wrap: row;
+  gap: 1vw;
+`;
+
+const OptionsSet = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1vh;
+  
+  font-size: max(2.5vh, 15px);
+`;
+
+const Button = styled.button`
+  display: flex;
+  min-width: 20vw;
+  min-height: 5vh;
+  
+  background: #f0f0f0;
+  border: solid 1px #aa00aa;
+  border-radius: 4px;
 `;
 
 export const Styled = {
@@ -47,4 +64,6 @@ export const Styled = {
     Column,
     Row,
     Options,
+    OptionsSet,
+    Button,
 }
--- frontend/src/src/TranslationPage.tsx
+++ frontend/src/src/TranslationPage.tsx
@@ -6,12 +6,12 @@ import {EnhancedTextArea } from "./components/EnchancedTextArea";
 import {Styled as T} from "./components/TextArea.styled";
 
 export const TranslationPage = () => {
-    const [test, setTest] = useState<string>();
-    useEffect(() => {
-        sendRequest<PromelaProgramDto>('http://localhost:8080/translate', {p: "name"}, "dvd").then(value => {
-            setTest(value.program);
-        });
-    }, []);
+    // const [test, setTest] = useState<string>();
+    // useEffect(() => {
+    //     sendRequest<PromelaProgramDto>('http://localhost:8080/translate', {p: "name"}, "dvd").then(value => {
+    //         setTest(value.program);
+    //     });
+    // }, []);
 
     const [ postCode, setPostCode ] = useState<string>("");
     const translatorOutput = useRef<HTMLTextAreaElement>(null);
@@ -24,31 +24,25 @@ export const TranslationPage = () => {
 
     return (
         <S.RootPageContainer>
-            <S.Column gap={10}>
-                <EnhancedTextArea
-                    className='code-input'
-                    tabSize={2}
-                    onTextChange={setPostCode}
-                />
-                <S.Options />
-                <S.Row stretchingAlong>
-                    <S.Column>
-                        <S.Options />
-                        <S.Options />
-                        <S.Options />
-                    </S.Column>
-                    <S.Column>
-                        <S.Options />
-                        <S.Options />
-                    </S.Column>
-                </S.Row>
-            </S.Column>
-            <S.Column gap={10}>
-                <T.TextArea ref={translatorOutput} />
-                <S.Row>
+            <EnhancedTextArea
+                className='code-input'
+                tabSize={2}
+                onTextChange={setPostCode}
+            />
+            <T.TextArea ref={translatorOutput} />
 
-                </S.Row>
-            </S.Column>
+            <S.Options>
+                <S.OptionsSet>
+                    <S.Button>Translate</S.Button>
+                </S.OptionsSet>
+                <S.OptionsSet>
+                    Examples:
+                    <S.Button>Empty</S.Button>
+                    <S.Button>Light</S.Button>
+                    <S.Button>Solar power station</S.Button>
+                </S.OptionsSet>
+            </S.Options>
+            <T.TextArea />
         </S.RootPageContainer>
     );
 }
